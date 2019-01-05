dbfaab8cc806fae3c49d2e84f46e7bf52de732b8
dontcrypls36 <dontcrypls36@google.com>
2019-01-05 10:17:19 +0300

    Add spinner to order-list;

--- src/app/components/order-list/order-list.component.ts
+++ src/app/components/order-list/order-list.component.ts
@@ -3,6 +3,7 @@ import { OrderService } from '../../services/order.service';
 import { OrderResponse } from '../../models/order-response';
 import { MatDialog } from '@angular/material';
 import { OrderDetailsComponent } from '../order-details/order-details.component';
+import { SpinnerServiceService } from '../../services/spinner-service.service';
 
 @Component({
   selector: 'app-order-list',
@@ -14,11 +15,18 @@ export class OrderListComponent implements OnInit {
   orders: OrderResponse[] = [];
 
   constructor(private orderService: OrderService,
-    private dialog: MatDialog) { }
+              private dialog: MatDialog,
+              private spinner: SpinnerServiceService) { }
 
   ngOnInit() {
+    this.load();
+  }
+
+  load() {
+    this.spinner.show();
     this.orderService.getAllOrders().subscribe( res => {
-       this.orders = res.OrderItems;
+      this.orders = res.OrderItems;
+      this.spinner.hide();
     });
   }
 
