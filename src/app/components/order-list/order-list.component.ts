623dfe07a1feef9a898fbf14a99a7fc4782287a2
dontcrypls36 <dontcrypls36@google.com>
2019-02-02 14:31:46 +0300

    Change orders list;

--- src/app/components/order-list/order-list.component.ts
+++ src/app/components/order-list/order-list.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {OrderService} from '../../services/order.service';
 import {OrderResponse} from '../../models/order-response';
-import {MatDialog, MatPaginator, MatTableDataSource} from '@angular/material';
+import {MatDialog} from '@angular/material';
 import {OrderDetailsComponent} from '../order-details/order-details.component';
 import {SpinnerServiceService} from '../../services/spinner-service.service';
 
@@ -12,10 +12,7 @@ import {SpinnerServiceService} from '../../services/spinner-service.service';
 })
 export class OrderListComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'number', 'outlet', 'shipdate', 'status', 'factquant' , 'factsum', 'comment'];
   orders: OrderResponse[] = [];
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  table: MatTableDataSource<OrderResponse>;
 
   constructor(private orderService: OrderService,
               private dialog: MatDialog,
@@ -29,13 +26,14 @@ export class OrderListComponent implements OnInit {
     this.spinner.show();
     this.orderService.getAllOrders().subscribe( res => {
       this.orders = res.OrderItems;
-      this.table = new MatTableDataSource<OrderResponse>(res.OrderItems);
-      this.table.paginator = this.paginator;
       this.spinner.hide();
     });
   }
 
   showOrderDetails(order: OrderResponse) {
+    if (order.TotalFactQuant <= 0) {
+      return;
+    }
     this.dialog.open(OrderDetailsComponent, {data: order});
   }
 
